4003bd5a4593161b59329c46da1e332d9d0743ab
workwhileweb <workwhileweb@gmail.com>
2022-11-23 00:01:57 +0700

    refactor

--- Program.cs
+++ Program.cs
@@ -3,37 +3,36 @@ using System.Collections.Generic;
 using System.Net;
 using System.Threading;
 
-namespace Turn
+namespace Turn;
+
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        private static void Main(string[] args)
+        if (args.Length <= 1)
         {
-            if (args.Length <= 1)
-            {
-                Console.WriteLine("Usage : exe [<IP:Port> <TurnedIP:TurnedPort>]...");
-                return;
-            }
-
-            var rules = new List<Tuple<IPEndPoint, IPEndPoint>>();
-            for (var i = 0; i < args.Length -1; i += 2)
-            {
-                var split0 = args[i + 0].Split(':');
-                var split1 = args[i + 1].Split(':');
-                var addr0 = IPAddress.Parse(split0[0]);
-                var addr1 = IPAddress.Parse(split1[0]);
-                var port0 = int.Parse(split0[1]);
-                var port1 = int.Parse(split1[1]);
-                var end0 = new IPEndPoint(addr0, port0);
-                var end1 = new IPEndPoint(addr1, port1);
-                var rule = new Tuple<IPEndPoint, IPEndPoint>(end0, end1);
-                rules.Add(rule);
-            }
-
-            var turn = new Turn(rules);
-            turn.Start();
-
-            Thread.Sleep(Timeout.Infinite);
+            Console.WriteLine("Usage : exe [<IP:Port> <TurnedIP:TurnedPort>]...");
+            return;
         }
+
+        var rules = new List<Tuple<IPEndPoint, IPEndPoint>>();
+        for (var i = 0; i < args.Length - 1; i += 2)
+        {
+            var split0 = args[i + 0].Split(':');
+            var split1 = args[i + 1].Split(':');
+            var addr0 = IPAddress.Parse(split0[0]);
+            var addr1 = IPAddress.Parse(split1[0]);
+            var port0 = int.Parse(split0[1]);
+            var port1 = int.Parse(split1[1]);
+            var end0 = new IPEndPoint(addr0, port0);
+            var end1 = new IPEndPoint(addr1, port1);
+            var rule = new Tuple<IPEndPoint, IPEndPoint>(end0, end1);
+            rules.Add(rule);
+        }
+
+        var turn = new Turn(rules);
+        turn.Start();
+
+        Thread.Sleep(Timeout.Infinite);
     }
 }
--- Turn.cs
+++ Turn.cs
@@ -4,151 +4,102 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 
-namespace Turn
+namespace Turn;
+
+public class Turn
 {
-    public class Turn
+    public Action<object>? Log;
+
+    public Turn(IEnumerable<Tuple<IPEndPoint, IPEndPoint>> rules)
     {
-        public Action<object>? Log;
+        Rules = new List<Tuple<IPEndPoint, IPEndPoint>>(rules);
+    }
 
-        protected List<Tuple<IPEndPoint, IPEndPoint>> Rules { get; }
+    protected List<Tuple<IPEndPoint, IPEndPoint>> Rules { get; }
 
-        protected List<TcpListener> TcpListeners { get; } = new();
+    protected List<TcpListener> TcpListeners { get; } = new();
 
-        protected bool Started { get; set; }
+    protected bool Started { get; set; }
 
-        public Turn(IEnumerable<Tuple<IPEndPoint, IPEndPoint>> rules)
+    /// <summary>
+    ///     Start
+    /// </summary>
+    public void Start()
+    {
+        if (Started) return;
+
+        var dict = new Dictionary<TcpListener, IPEndPoint>();
+
+        TcpListeners.Clear();
+
+        foreach (var turn in Rules)
         {
-            Rules = new List<Tuple<IPEndPoint, IPEndPoint>>(rules);
+            var tl = new TcpListener(turn.Item1);
+            TcpListeners.Add(tl);
+            dict.Add(tl, turn.Item2);
+            tl.Start();
         }
 
-        /// <summary>
-        /// Start
-        /// </summary>
-        public void Start()
-        {
-            if (Started) return;
+        Started = true;
 
-            var dict = new Dictionary<TcpListener, IPEndPoint>();
+        foreach (var tl in TcpListeners) Loop(tl, dict[tl]);
+    }
 
-            TcpListeners.Clear();
+    /// <summary>
+    ///     Close All Connections And Stop
+    /// </summary>
+    // ReSharper disable once UnusedMember.Global
+    public void Stop()
+    {
+        if (!Started) return;
 
-            foreach (var turn in Rules)
+        foreach (var tl in TcpListeners)
+            try
             {
-                var tl = new TcpListener(turn.Item1);
-                TcpListeners.Add(tl);
-                dict.Add(tl, turn.Item2);
-                tl.Start();
+                tl.Stop();
+            }
+            catch
+            {
+                // ignored
             }
 
-            Started = true;
+        Started = false;
+    }
 
-            foreach (var tl in TcpListeners)
-            {
-                Loop(tl, dict[tl]);
-            }
-        }
-
-        /// <summary>
-        /// Close All Connections And Stop
-        /// </summary>
-        // ReSharper disable once UnusedMember.Global
-        public void Stop()
+    /// <summary>
+    ///     Accept Connections To tl, And Redirect Data To target
+    /// </summary>
+    /// <param name="tl"></param>
+    /// <param name="target"></param>
+    protected async void Loop(TcpListener tl, IPEndPoint target)
+    {
+        while (Started)
         {
-            if (!Started) return;
-
-            foreach (var tl in TcpListeners)
-            {
-                try
-                {
-                    tl.Stop();
-                }
-                catch
-                {
-                    // ignored
-                }
-            }
-
-            Started = false;
-        }
-
-        /// <summary>
-        /// Accept Connections To tl, And Redirect Data To target
-        /// </summary>
-        /// <param name="tl"></param>
-        /// <param name="target"></param>
-        protected async void Loop(TcpListener tl, IPEndPoint target)
-        {
-            while (Started)
-            {
-                TcpClient? tc0 = null;
-                TcpClient? tc1 = null;
-
-                try
-                {
-                    tc0 = await tl.AcceptTcpClientAsync().ConfigureAwait(false);
-                    
-                    tc1 = new TcpClient();
-                    await tc1.ConnectAsync(target.Address, target.Port).ConfigureAwait(false);
-
-                    Connect(tc0, tc1);
-                    Connect(tc1, tc0);
-
-                    Log?.Invoke(new
-                    {
-                        Connection = "Established",
-                        IPAddress = (tl.LocalEndpoint as IPEndPoint)?.ToString(),
-                        target,
-                    });
-                }
-                catch
-                {
-                    try
-                    {
-                        tc0?.Close();
-                    }
-                    catch
-                    {
-                        // ignored
-                    }
-
-                    try
-                    {
-                        tc1?.Close();
-                    }
-                    catch
-                    {
-                        // ignored
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        /// Read Data From tc0 And Write To tc1
-        /// </summary>
-        /// <param name="tc0"></param>
-        /// <param name="tc1"></param>
-        protected async void Connect(TcpClient tc0, TcpClient tc1)
-        {
-            Stream s0 = tc0.GetStream();
-            Stream s1 = tc1.GetStream();
-
-            var buffer = new byte[ushort.MaxValue];
+            TcpClient? tc0 = null;
+            TcpClient? tc1 = null;
 
             try
             {
-                while (Started)
+                tc0 = await tl.AcceptTcpClientAsync().ConfigureAwait(false);
+
+                tc1 = new TcpClient();
+                await tc1.ConnectAsync(target.Address, target.Port).ConfigureAwait(false);
+
+                Connect(tc0, tc1);
+                Connect(tc1, tc0);
+
+                Log?.Invoke(new
                 {
-                    var count = await s0.ReadAsync(buffer).ConfigureAwait(false);
-                    await s1.WriteAsync(buffer.AsMemory(0, count)).ConfigureAwait(false);
-                }
+                    Connection = "Established",
+                    IPAddress = (tl.LocalEndpoint as IPEndPoint)?.ToString(),
+                    target
+                });
             }
             catch
             {
                 try
                 {
-                    tc0.Close();
-                    Log?.Invoke(new { Closed = tc0.Client.LocalEndPoint });
+                    tc0?.Close();
                 }
                 catch
                 {
@@ -157,13 +108,56 @@ namespace Turn
 
                 try
                 {
-                    tc1.Close();
-                    Log?.Invoke(new { Closed = tc1.Client.LocalEndPoint });
+                    tc1?.Close();
                 }
                 catch
                 {
                     // ignored
                 }
+            }
+        }
+    }
+
+    /// <summary>
+    ///     Read Data From tc0 And Write To tc1
+    /// </summary>
+    /// <param name="tc0"></param>
+    /// <param name="tc1"></param>
+    protected async void Connect(TcpClient tc0, TcpClient tc1)
+    {
+        Stream s0 = tc0.GetStream();
+        Stream s1 = tc1.GetStream();
+
+        var buffer = new byte[ushort.MaxValue];
+
+        try
+        {
+            while (Started)
+            {
+                var count = await s0.ReadAsync(buffer).ConfigureAwait(false);
+                await s1.WriteAsync(buffer.AsMemory(0, count)).ConfigureAwait(false);
+            }
+        }
+        catch
+        {
+            try
+            {
+                tc0.Close();
+                Log?.Invoke(new { Closed = tc0.Client.LocalEndPoint });
+            }
+            catch
+            {
+                // ignored
+            }
+
+            try
+            {
+                tc1.Close();
+                Log?.Invoke(new { Closed = tc1.Client.LocalEndPoint });
+            }
+            catch
+            {
+                // ignored
             }
         }
     }
